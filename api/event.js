147b7941f079c3887938ba0cce3386087ed80880
drfengyu <98081672+drfengyu@users.noreply.github.com>
2024-01-20 15:50:10 +0800

    Update event.js

--- api/event.js
+++ api/event.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const axios = require('axios');
 app.use(bodyParser.json());
 app.post('/api/event', (req, res) => {
   if (req.body.hasOwnProperty('challenge')) {
@@ -8,5 +9,33 @@ app.post('/api/event', (req, res) => {
   } else {
     res.status(400).send('No challenge found in request');
   }
+  const eventType = req.body.type;
+  let message = '';
+  switch (eventType) {
+    case 'im.chat.member.bot.added_v1':
+      // 生成机器人被添加到群聊的通知
+      message = `机器人被添加到一个群聊中。`;
+      break;
+    case 'im.chat.member.user.added_v1':
+      // 生成用户被添加到群聊的通知
+      message = `${req.body.user.name} 被添加到一个群聊中。`;
+      break;
+    case 'im.message.message_read_v1':
+      // 生成消息已读的通知
+      message = `${req.body.user.name} 已读一条消息。`;
+      break;
+    case 'im.message.receive_v1':
+      // 生成接收到新的群聊消息的通知
+      message = `接收到新的群聊消息：${req.body.text.content}`;
+      break;
+    default:
+      console.log(`收到未支持的事件类型：${eventType}`);
+      return res.status(200).send('OK');
+  }
+  // 在Discord中发布通知
+  axios.post(process.env.DISCORD_WEBHOOK_URL, {content: message})
+    .then(() => console.log('Discord通知已发送！'))
+    .catch(err => console.error(err));
+  return res.status(200).send('OK');
 });
 module.exports = app;
