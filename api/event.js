b6a205ebfb2e47924fa70568985a9c3fed7f9b3e
drfengyu <98081672+drfengyu@users.noreply.github.com>
2024-01-20 15:50:47 +0800

    Update event.js

--- api/event.js
+++ api/event.js
@@ -3,12 +3,12 @@ const app = express();
 const bodyParser = require('body-parser');
 const axios = require('axios');
 app.use(bodyParser.json());
-app.post('/api/event', (req, res) => {
-  if (req.body.hasOwnProperty('challenge')) {
-    res.status(200).send({ challenge: req.body.challenge });
-  } else {
-    res.status(400).send('No challenge found in request');
-  }
+//  app.post('/api/event', (req, res) => {
+//  if (req.body.hasOwnProperty('challenge')) {
+//    res.status(200).send({ challenge: req.body.challenge });
+//  } else {
+//    res.status(400).send('No challenge found in request');
+//  }
   const eventType = req.body.type;
   let message = '';
   switch (eventType) {
