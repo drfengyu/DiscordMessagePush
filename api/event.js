c9bea32d135bebaf305ae24fd575e153b368032d
drfengyu <98081672+drfengyu@users.noreply.github.com>
2024-01-20 14:51:21 +0800

    Update event.js

--- api/event.js
+++ api/event.js
@@ -1,10 +1,20 @@
 module.exports = async (req, res) => {
-  let body = '';
-  req.on('data', chunk => {
-    body += chunk.toString();
-  });
-  req.on('end', () => {
-    const { challenge } = JSON.parse(body);
-    res.status(200).send(challenge);
-  });
+    const { method, headers, url } = req;
+    let body = [];
+    req.on('data', chunk => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        // 获取 POST 请求中的 JSON 数据
+        let parsedBody = JSON.parse(body);
+        // 检查是否存在 challenge 属性
+        if (parsedBody.hasOwnProperty('challenge')) {
+            let { challenge } = parsedBody;
+            // 返回同样的 challenge
+            res.status(200).send(challenge);
+        } else {
+            // 没有找到 challenge 属性
+            res.status(400).send('No challenge specified in request.');
+        }
+    });
 };
